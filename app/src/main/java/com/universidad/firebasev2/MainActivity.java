8feca0dac3296fe842d8b4928b0e58c224664853
felip <mccomander12@gmail.com>
2022-10-20 14:53:57 -0400

    clase 1 fire base

---
+++ app/src/main/java/com/universidad/firebasev2/MainActivity.java
@@ -0,0 +1,34 @@
+package com.universidad.firebasev2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnAgregar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAgregar = (Button) findViewById(R.id.btnAgregar);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hola", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+}
